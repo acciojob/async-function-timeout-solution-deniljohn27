888644f73adcd727db99be1608b7c7e490aef5ab
deniljohn27 <117897719+deniljohn27@users.noreply.github.com>
2023-05-28 22:37:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ const btn = document.getElementById("btn");
 const output = document.getElementById("output");
 
 //your code here
-async function showMessage() {
+function showMessage() {
   const message = text.value;
   const delayVal = delay.value;
-  await new Promise((resolve) => setTimeout(resolve, delayVal)).then(()=>{	
-  output.innerText = message;});
+  // await new Promise((resolve) => setTimeout(resolve(), delayVal)).then(()=>{	
+  // output.innerText = message;});
+	setTimeout(()=>{output.innerText = message;},delayVal)
 
 }
 
